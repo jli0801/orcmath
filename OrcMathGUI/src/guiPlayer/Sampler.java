0f788b91ae01d52a3532d41d00bacb81deadfbc6
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-12-14 15:36:02 -0500

    yay rectangle

--- OrcMathGUI/src/guiPlayer/Sampler.java
+++ OrcMathGUI/src/guiPlayer/Sampler.java
@@ -119,7 +119,7 @@ public class Sampler extends GUIApplication {
 				}
 			}); 
 			viewObjects.add(cb);
-			
+			viewObjects.add(new Pigeon());
 		}
 		
 		public void mouseDragged(MouseEvent m) {
