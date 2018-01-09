522ea0f7fe2f1f719127cc4f385a490f27cbce19
Jessica Li <jli0801@bths.edu>
2018-01-09 09:43:18 -0500

    Update ProgressVincent.java

--- OrcMathGUI/src/myStuff/ProgressVincent.java
+++ OrcMathGUI/src/myStuff/ProgressVincent.java
@@ -16,14 +16,26 @@ public class ProgressVincent extends Component implements ProgressInterfaceJessi
 
 	@Override
 	public void add(Object randomMove) {
-		// TODO Auto-generated method stub
-
+		sequenceNum++;
 	}
 
 	@Override
 	public void update(Graphics2D g) {
-		// TODO Auto-generated method stub
-
+		clear();
+		if(loss) {
+			g.setColor(Color.RED);
+			g.fillRect(300, 0, 200, 100);
+			g.setColor(Color.black);
+			g.drawString("Round: "+roundNum,10,55);
+			g.drawString("Current Sequence Length: "+sequenceNum,5,100);
+			g.drawString("LOL YOU CAN'T EVEN PLAY A KID GAME",5,150);
+		}else {
+			g.setColor(Color.GREEN);
+			g.fillRect(300, 0, 200, 100);
+			g.setColor(Color.black);
+			g.drawString("Round: "+roundNum,5,55);
+			g.drawString("Current Sequence Length: "+sequenceNum,5,100);
+		}
 	}
 	
 	public void gameOver()
